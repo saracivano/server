3716756ee3da8fe115301427b91a99330573854e
Ivano Šarac <saracivano32@gmail.com>
2024-04-26 07:39:34 +0000

    Post method test implementation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import cgi
 
 HOST = "localhost"
 PORT = 8000
@@ -15,6 +16,18 @@ class Server(BaseHTTPRequestHandler):
         })
 
         self.wfile.write(bytes(response, "utf-8"))
+    
+    def do_POST(self):
+        if self.path == "/python3":
+            content_length = int(self.headers['Content-Length'])
+            post_data_bytes = self.rfile.read(content_length)
+
+            post_data_str = post_data_bytes.decode("utf-8")
+            list_of_post_data = post_data_str.split('&')
+            print(post_data_str)
+
+            self.send_response(204)
+
 
 if __name__ == "__main__":
     server = HTTPServer((HOST, PORT), Server)
